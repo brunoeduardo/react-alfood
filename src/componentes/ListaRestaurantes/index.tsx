4245f94feaf9efc920bb3214e994993b752e36bb
Bruno Eduardo <bruno@beduardo.com>
2024-07-23 14:29:44 -0300

    lista de restaurante filtro

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -5,11 +5,17 @@ import Restaurante from './Restaurante';
 import axios from 'axios';
 import { IPaginacao } from '../../interfaces/IPaginacao';
 
+interface IPesquisa {
+  search?: string
+  ordering?: string
+}
+
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [nextPage, setNextPage] = useState<string>("")
   const [previous, setPrevious] = useState<string>("")
+  const [filtro, setFiltro] = useState<string>("")
 
   useEffect(() => {
     axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
@@ -44,8 +50,27 @@ const ListaRestaurantes = () => {
       .catch(error => console.log(error))
   }
 
+  const aplicarFiltro = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios.get<IPaginacao<IRestaurante>>(`http://localhost:8000/api/v1/restaurantes/?search=${filtro}`)
+      .then(result => {
+        setRestaurantes(result.data.results)
+        setNextPage(result.data.next)
+        setPrevious(result.data.previous)
+      })
+      .catch(error => console.log(error))
+
+  }
+
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <form onSubmit={aplicarFiltro}>
+      <input type="text" onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+        setFiltro(event.target.value);
+      }} />
+      <button type='submit'>Filtrar</button>
+    </form>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
 
     <button onClick={anteriorPagina} disabled={!previous}>Anterior</button>
